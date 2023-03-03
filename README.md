9f256ff1e18b9f46869d406d77360c3746c82d9b
M.Inzamam Malik <malikasinger1@gmail.com>
2023-03-03 21:50:00 +0500

    leaving from workspace

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # google-speech-api-from-browser
 
-
-referances:
+references:
 
 1) https://medium.com/google-cloud/building-your-own-conversational-voice-ai-with-dialogflow-speech-to-text-in-web-apps-part-i-b92770bd8b47
 2) https://medium.com/google-cloud/building-a-client-side-web-app-which-streams-audio-from-a-browser-microphone-to-a-server-part-ii-df20ddb47d4e
@@ -9,3 +8,13 @@ referances:
 4) https://medium.com/google-cloud/getting-audio-data-from-text-text-to-speech-and-play-it-in-your-browser-part-iv-cd2d6ea71c6a
 
 making a working example is due
+
+
+
+auto stop on silence with hark.js
+https://www.npmjs.com/package/hark
+sample: https://github.com/muaz-khan/RecordRTC/blob/master/simple-demos/auto-stop-on-silence.html
+
+
+
+conclusion: it is not supposed to work from browser, audio stream will goto server first and then goto google cloud api
--- google-speech-api-reactnodejs/src/App.jsx
+++ google-speech-api-reactnodejs/src/App.jsx
@@ -4,6 +4,15 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { io } from 'socket.io-client';
 import RecordRTC from "recordrtc";
+import hark from "hark";
+import getUserMedia from 'getusermedia'
+import Siriwave from 'react-siriwave';
+
+
+
+
+
+
 
 
 function App() {
@@ -18,6 +27,7 @@ function App() {
   const [isSocketConnected, setIsSocketConnected] = useState(true);
   const [recordAudio, setRecordAudio] = useState({})
   const [file, setFile] = useState(null)
+  const [amplitude, setAmplitude] = useState(0)
 
   useEffect(() => {
     if (isRecording) {
@@ -43,6 +53,40 @@ function App() {
         });
 
       }, 3000);
+
+
+      getUserMedia({ video: false, audio: true }, function (err, stream) {
+        // if the browser doesn't support user media
+        // or the user says "no" the error gets passed
+        // as the first argument.
+        if (err) {
+          console.log('failed');
+        } else {
+          console.log('got a stream', stream);
+
+          var speechEvents = hark(stream, {});
+
+          speechEvents.on('speaking', function (e) {
+            console.log('speaking', e);
+            speechEvents.on('volume_change', function (e) {
+              // console.log('volume_change', e);
+              let amplitude = 3 - (e - -40) / -10; // scaling -50 - -20 to 0 - 3
+
+              if (amplitude < 0) amplitude = 0.5;
+              if (amplitude > 4) amplitude = 4;
+
+              // console.log(e, ' amplitude ', amplitude);
+              setAmplitude(amplitude)
+            });
+          });
+
+          speechEvents.on('stopped_speaking', function (e) {
+            console.log('stopped_speaking', e);
+            speechEvents.stop();
+            setAmplitude(prev => 0)
+          });
+        }
+      });
     }
   }, [isRecording]);
 
@@ -75,29 +119,56 @@ function App() {
   }
 
 
+
+
   return (
     <div className="App">
+      {amplitude}
+
+      <Siriwave style='ios9' lerpSpeed={0.2} speed={0.3} amplitude={amplitude} />
+
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        height: "200px",
+        alignItems: "center",
+      }}>
+        <div style={{
+          borderWidth: amplitude * 20,
+          borderColor: 'white',
+          borderStyle: 'solid',
+          minHeight: "50px",
+          minWidth: "50px",
+          backgroundColor: "red",
+          borderRadius: "50%"
+        }}></div>
+
+      </div>
 
       <button
         onClick={async () => {
           const audioRecorder = await startRec()
 
-
           setRecordAudio(audioRecorder)
           setIsRecording(true);
+
+
+
+
+
         }}
         disabled={isRecording || !isSocketConnected}>Start recording</button>
 
       {isRecording ? <p>Recording audio....</p> : null}
       {!isSocketConnected ? <p>Connecting to server....</p> : null}
-      
-      <br />
-      <br />
-      <br />
-      { file?.audio?.dataURL ? <audio controls src={file?.audio?.dataURL}></audio> : null}
-      
 
-    </div>
+      <br />
+      <br />
+      <br />
+      {file?.audio?.dataURL ? <audio controls src={file?.audio?.dataURL}></audio> : null}
+
+
+    </div >
   );
 }
 
